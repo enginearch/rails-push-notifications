3ef870af03c09a5b1fddaf4905b0aaca60eaf7fa
johnny <jan.piotr.latko@gmail.com>
2017-12-01 10:35:24 +0100

    debugging

--- lib/rails-push-notifications/apps/base_app.rb
+++ lib/rails-push-notifications/apps/base_app.rb
@@ -16,16 +16,23 @@ module RailsPushNotifications
     # push them.
     #
     def push_notifications
+      puts "In push notifications"
       pending = find_pending
+      logger.warn "Pending length: #{pending.length}"
       pending.update_all(processing: true) # in case other threads want to do that too
+      p "updated processing"
       to_send = pending.map do |notification|
         notification_type.new notification.destinations, notification.data
       end
+      logger.warn "To send length: #{to_send.length}"
       pusher = build_pusher
       pusher.push to_send
+      logger.warn "after push"
       pending.each_with_index do |p, i|
+        logger.warn "update with: #{results}"
         p.update_attributes! results: to_send[i].results, processing: false
       end
+      puts "After push notifications"
     end
 
     protected
